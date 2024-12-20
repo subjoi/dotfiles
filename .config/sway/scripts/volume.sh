9035310d34f762a551082eefdf74880e2f718faa
subjoi <ogsensi@protonmail.com>
2024-12-20 17:31:54 +0100

    -

--- .config/sway/scripts/volume.sh
+++ .config/sway/scripts/volume.sh
@@ -1,28 +1,32 @@
 #!/usr/bin/env bash
 
-VOLUME=$(pamixer --get-volume)
-MUTED=$(pamixer --get-mute)
+SINK="@DEFAULT_SINK@"
+
+MUTED=$(wpctl status -n | grep -E -i "(output).*(muted)")
+
+VOLUME=$(wpctl get-volume @DEFAULT_SINK@ | grep -E -o '(0\...)' -)
+$((VOLUME * 100))
 
 muted(){
-if [[ "$MUTED" == "true" ]]; then
+if [[ -n "$MUTED" ]]; then
   return 0
 fi
 }
 
 if [[ $1 == "--inc" ]]; then
-    pamixer --increase 5 --unmute && \
+    wpctl set-volume $SINK 5%+ && set-mute $SINK 0 
     notify-send --app-name=volume-control --replace-id=69 --category=device \
       -i "/usr/share/icons/Papirus-Dark/48x48/status/notification-audio-volume-high.svg" \
                "Volume Up:" "$VOLUME%"
 
 elif [[ $1 == "--dec" ]]; then
-    pamixer --decrease 5 --unmute && \
+    wpctl set-volume $SINK 5%- && set-mute $SINK 0
     notify-send --app-name=volume-control --replace-id=69 --category=device \
       -i "/usr/share/icons/Papirus-Dark/48x48/status/notification-audio-volume-medium.svg" \
       "Volume Down:" "$VOLUME%"
 
 elif [[ $1 == "--mute" ]]; then
-    pamixer --toggle-mute && \
+    wpctl set-mute $SINK toggle
       if muted; then
           notify-send --app-name=volume-control --replace-id=69 --category=device \
             -i "/usr/share/icons/Papirus-Dark/48x48/status/notification-audio-volume-muted.svg" \
