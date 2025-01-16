2026762d3ba97ffc51a5bbc25ae931a9c260c6bf
subjoi <ogsensi@protonmail.com>
2025-01-16 06:55:46 +0100

    new printscreen script. uses swappy and changed host to https://u-b.ad

---
+++ .config/sway/scripts/prntscrn.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+saved_dir="$HOME/pics/screens/$(date +%y-%m-%d%s).png"
+
+grim -t png -g "$(slurp)" $saved_dir
+swappy -f $saved_dir -o $saved_dir | wl-copy
+
+if [[ $? -eq 0 ]]; then
+  curl -F "file=@${saved_dir}" -Fsecret= u-b.ad | wl-copy
+  notify-send "Screenshot uploaded" "$(wl-paste)\n${saved_dir##*/}"
+fi
+
+if [[ $? -ne 0 ]]; then
+  notify-send "Error" "Screenshot upload failed!"
+  exit 1
+fi
