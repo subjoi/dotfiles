3f1d2674a12347dd6bf2ad1734e2fc913354ba45
subjoi <ogsensi@protonmail.com>
2025-05-03 15:59:00 +0200

    sway

--- .config/sway/scripts/prntscrn.sh
+++ .config/sway/scripts/prntscrn.sh
@@ -6,7 +6,7 @@ grim -t png -g "$(slurp)" $saved_dir
 swappy -f $saved_dir -o $saved_dir | wl-copy
 
 if [[ $? -eq 0 ]]; then
-  curl -F "file=@${saved_dir}" -Fsecret= https://u-b.ad | wl-copy
+  curl -F "file=@${saved_dir}" -Fsecret= -Fexpires=1  https://0x0.st | wl-copy
   notify-send "Screenshot uploaded" "$(wl-paste)\n${saved_dir##*/}"
 fi
 
